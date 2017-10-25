69b27a3e238adb821cf7cfae2531a65c365313e6
Alex Preda <alexpreda95@yahoo.com>
2017-10-25 19:34:06 +0300

    am redefinit din nou

--- senzors.js
+++ senzors.js
@@ -1,21 +1,25 @@
-document.getElementById("id_logic_level_version").innerHTML="Logic level version 2017.10.25.3";
+document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.10.25.3";
 window.addEventListener('deviceorientation', ondeviceorientation);
+
 window.addEventListener('devicemotion', ondevicemotion);
-//-------------------------------------------------------
- function ondeviceorientation(event) {
-	document.getElementById("id_alpha").innerHTML = " Alpha = " + Math.round(event.alpha * 10) / 10;
-	document.getElementById("id_beta").innerHTML = " Beta = " + Math.round(event.beta * 10) / 10;
-	document.getElementById("id_gamma").innerHTML = " Gamma = " + Math.round(event.gamma * 10) / 10;
-	 
+
+//---------------------------------------------------------------
+function ondeviceorientation(event) 
+{ 
+	document.getElementById("id_alpha").innerHTML ="alpha = " +  Math.round(event.alpha * 10) / 10;
+	document.getElementById("id_beta").innerHTML ="beta= " + Math.round(event.beta * 10) / 10;
+	document.getElementById("id_gamma").innerHTML ="gama= " + Math.round(event.gamma * 10) / 10;
 }
-//-------------------------------------------------------
+//---------------------------------------------------------------
 function ondevicemotion(event)
 {
-	document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10 + " " + Math.round(event.acceleration.z * 10) / 10;
-	
+	document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10 + " " + Math.round(event.acceleration.z * 10) / 10
 	var ag = event.accelerationIncludingGravity;
-	var gama = -Math.atan(ag.x / ag.z) * 180 / Math.PI;
+
+	var gamma = -Math.atan(ag.x / ag.z) * 180 / Math.PI;
 	var beta = Math.atan(ag.y / ag.z) * 180 / Math.PI;
-	
-	document.getElementById("id_acc_g").innerHTML ="Acc_g = " + Math.round(ag.x * 10) / 10 + " " + Math.round(ag.y * 10) / 10 + " " + Math.round(ag.z * 10) / 10 + "Gama = " + Math.round(gamma * 10) / 10 + " Beta = " + Math.round(beta * 10) / 10;		
+
+	document.getElementById("id_acc_g").innerHTML = "AccG = " + Math.round(ag.x * 10) / 10 + " " + Math.round(ag.y * 10) / 10 + " " + Math.round(ag.z * 10) / 10 + "gamma = " +  Math.round(gamma*10)/10 + "beta " +  Math.round(beta*10)/10;
+
 }
+//---------------------------------------------------------------
