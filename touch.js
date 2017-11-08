081afd574f02904efa357c640eea1aecfcfddf20
Alex Preda <alexpreda95@yahoo.com>
2017-11-08 17:42:49 +0200

    Rezolvare bug

--- touch.js
+++ touch.js
@@ -8,7 +8,7 @@ window.addEventListener("touchstart", on_touch_start);
 function on_touch_start(e)
 {
 	var touches = e.changedTouches;
-	for (i=0; i < touches.length, i++)
+	for (i=0; i < touches.length; i++)
 		{
 			context.beginPath();
 		    context.arc(touches[i].pageX, touches[i].pageY, 10, 0, Math.PI);
