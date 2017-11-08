63b7b847f4da0eeba713d0463873197c870df666
Alex Preda <alexpreda95@yahoo.com>
2017-11-08 17:51:21 +0200

    O noua modificare

--- touch.js
+++ touch.js
@@ -1,9 +1,9 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.1";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.2";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
-window.addEventListener("touchstart", on_touch_start);
+canvas.addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e)
 {
@@ -11,7 +11,7 @@ function on_touch_start(e)
 	for (i=0; i < touches.length; i++)
 		{
 			context.beginPath();
-		    context.arc(touches[i].pageX, touches[i].pageY, 10, 0, Math.PI);
+		    context.arc(touches[i].pageX, touches[i].pageY, 10, 0, 2 * Math.PI);
 			context.stroke();
 		}
 }
