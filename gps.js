6d66bf29c7894494f246f7bc70e63e38c180b14f
Alex Preda <alexpreda95@yahoo.com>
2017-12-06 17:48:37 +0200

    Adding altitude

--- gps.js
+++ gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_level_version").innerHTML="Business level version 2017.12.06.1";
+document.getElementById("id_business_level_version").innerHTML="Business level version 2017.12.06.2";
 
 
 navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
@@ -6,7 +6,7 @@ navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
 //----------------------------------------------
 function on_gps_success(p)
 {
-	document.getElementById("id_p").innerHTML = "Lat = " + p.coords.latitude + " Long = " + p.coords.longitude + " Accuracy = " + p.coords.accuracy + "m";
+	document.getElementById("id_p").innerHTML = "Lat = " + p.coords.latitude + " <br> Long = " + p.coords.longitude + "<br> Accuracy = " + p.coords.accuracy + "m" + "<br> Altitude = " + p.coords.altitude + "m";
 } 
 //-----------------------------------------------
 function on_gps_error(e)
